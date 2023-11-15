da43ad063a1185b54b4ee16d78801fc4356cc8b9
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-11-15 15:40:03 -0500

    Vite template and adding files to begin JSX components

---
+++ myProfile/src/header.jsx
@@ -0,0 +1,10 @@
+<header>
+    <h1 class="hero-text">I am Lisa Eimicke</h1>
+
+    <nav class="navigation">
+        <a  href="#ContactMe">Contact Me</a>
+        <a  href="#Work">Work</a>
+        <a  href="#AboutMe">About Me</a>
+    </nav>
+    <img src="./Assets/images/my_profile.jpg" alt="Picture of Lisa Eimicke" class="image">
+</header>
