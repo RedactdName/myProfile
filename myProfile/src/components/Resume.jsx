346f3b110ca7203827ea807de7e0675a29139f35
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-12-11 11:35:28 -0700

    debugg Resume.jsx and build error

--- myProfile/src/components/Resume.jsx
+++ myProfile/src/components/Resume.jsx
@@ -1,21 +1,11 @@
-import Resume from '../../src/pdf/Resume.pdf'
-// export default function Resume() {
-//     return (
-//         <div>
-//             <p>Here is a link to my Resume!</p>
-//             <iframe src={Resume} 
-//             width='75%' height='1000rem'>
-//             </iframe>
-//         </div>
-//     );
-//   }
+
 function Resume() {
   return (
     <section>
       <h1>Lisa Eimicke's Resume</h1>
       <p>
         <a href="../../src/pdf/Resume.pdf" target="_blank">
-          Resume
+          Click Here to View My Resume
         </a>
       </p>
     </section>
